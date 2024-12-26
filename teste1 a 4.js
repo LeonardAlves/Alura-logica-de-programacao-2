0533e1c93c5b92bcd270d95661913f107f31487b
Leonardo Alves <85313751+LeonardAlves@users.noreply.github.com>
2024-12-26 16:36:37 +0000

    Add files via upload

---
+++ teste1 a 4.js
@@ -0,0 +1,13 @@
+alert("Boas Vindas");
+
+let nome;
+let pergunta;
+
+
+nome = prompt("Digite seu nome");
+alert(`Bem vindo ${nome}`);
+console.log(nome);
+
+pergunta = prompt("Qual a linguagem de programção que você mais gosta?");
+console.log(pergunta);
+
---
+++ teste5.js
@@ -0,0 +1,19 @@
+let valor1; 
+let valor2;
+let resultado;
+
+
+valor1 = parseInt
+valor2 = parseInt
+alert("Calculadora Artificial, escolha dois numeros para serem somados");
+valor1 = prompt("Digite um valor");
+console.log(valor1);
+
+valor2 = prompt("Digite o segundo valor");
+console.log(valor2);
+
+resultado = (valor1 + valor2);
+console.log(resultado);
+alert(resultado);
+
+;
